baa54fe7025e63bad242057916b2ee4228da458e
Eino Tick <einotick@yahoo.com>
2024-06-17 20:42:57 +0300

    update styles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import PageHeader from "../layouts/PageHeader.tsx";
 
 const HomePage = props => {
     let nolla = 0;
     nolla += 0
 
     return (
-        <div>
-
+        <div className="max-h-screen flex flex-col">
+            <PageHeader title="Home"/>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     );
 };
 
-HomePage.propTypes = {
-
-};
+HomePage.propTypes = {};
 
 export default HomePage;
