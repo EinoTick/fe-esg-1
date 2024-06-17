cc4e4c733883baf8ee9dc7a70177caff8fda4dfe
Eino Tick <einotick@yahoo.com>
2024-06-17 20:41:10 +0300

    add font

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HomePage = props => {
+    let nolla = 0;
+    nolla += 0
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+HomePage.propTypes = {
+
+};
+
+export default HomePage;
