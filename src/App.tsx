6e3abf2a739229ee8db916707f533dec079814ed
Eino Tick <einotick@yahoo.com>
2024-06-17 20:42:40 +0300

    clear app file

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
+import PageHeader from "./layouts/PageHeader.tsx";
+import Navbar from "./components/Navbar.tsx";
+import HomePage from "./pages/HomePage.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
+          <Navbar />
+          <HomePage />
       </>
   )
 }
